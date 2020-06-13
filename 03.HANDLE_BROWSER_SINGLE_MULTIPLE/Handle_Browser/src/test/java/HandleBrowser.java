e39edb88ce6d5ee6ed7bbba47ee2888227685619
sagarika112 <58979217+sagarika112@users.noreply.github.com>
2020-06-13 21:11:27 +0530

    Independent window handle done

--- 03.HANDLE_BROWSER_SINGLE_MULTIPLE/Handle_Browser/src/test/java/HandleBrowser.java
+++ 03.HANDLE_BROWSER_SINGLE_MULTIPLE/Handle_Browser/src/test/java/HandleBrowser.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -6,27 +7,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HandleBrowser {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
 				 "C:\\Users\\Admin\\eclipse-workspace\\Banking-test\\Drivers\\chromedriver_win32 (6)\\chromedriver.exe" ); 
 		WebDriver driver= new ChromeDriver();
-		//driver.get("https://chercher.tech/python/windows-selenium-python");
 		driver.get("http://www.naukri.com/");
 		 // It will return the parent window name as a String
 		 String mainWindow=driver.getWindowHandle();
 		 // It returns no. of windows opened by WebDriver and will return Set of Strings
-		 Set<String> set =driver.getWindowHandles();
-		 // Using Iterator to iterate with in windows
-		 Iterator<String> itr= set.iterator();
-		 while(itr.hasNext()){
-		 String childWindow=itr.next();
-		    // Compare whether the main windows is not equal to child window. If not equal, we will close.
-		 if(!mainWindow.equals(childWindow)){
-		 driver.switchTo().window(childWindow);
-		 System.out.println(driver.switchTo().window(childWindow).getTitle());
+		 Thread.sleep(2000);
+		 ArrayList<String> ll= new ArrayList<String>(driver.getWindowHandles());
+         System.out.println(ll);
+		 String Window1=ll.get(0);
+		 String Window2=ll.get(1);
+		 String Window3=ll.get(2);
+		 driver.switchTo().window(Window1);
+		 System.out.println(driver.getTitle());
+		 driver.switchTo().window(Window2);
+		 System.out.println(driver.getTitle());
+		 driver.switchTo().window(Window3);
+		 System.out.println(driver.getTitle());
+		 driver.switchTo().window(mainWindow);
+		 System.out.println(driver.getTitle());
+		
 		 driver.close();
 	}
+}
 
-}
-}
-}
+
